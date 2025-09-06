2d1f68def8c38c63d3ef876918376adfd0831298
AVanessa208 <vanesisitye@gmail.com>
2025-09-06 00:35:02 +0000

    Subir parcial

--- parcial-4/estructural.py
+++ parcial-4/estructural.py
@@ -8,4 +8,50 @@ def invertir_lista(lista):
         lista[len(lista)-i-1] = temp
     return lista
 
-print(invertir_lista([1,2,3,4,5])
+print(invertir_lista([1,2,3,4,5,6,7,8,9,10,11]))
+
+#Suma y numero maximo 
+
+def suma_y_maximo(lista):
+    if not lista:
+        return 0, None
+    
+    suma = 0
+    maximo = lista[0]
+    
+    for numero in lista:
+        suma += numero
+        if numero > maximo:
+            maximo = numero
+            
+    return suma, maximo
+
+numeros = [1,2,3,4,5,6,7,8,9,10,11]
+suma_total, numero_maximo = suma_y_maximo(numeros)
+print(f"La suma es: {suma_total}")
+print(f"El número más alto es: {numero_maximo}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- parcial-4/oop.py
+++ parcial-4/oop.py
@@ -13,3 +13,49 @@ class CuentaBancaria:
 
 c = CuentaBancaria('Ana', 100)
 c.retirar(150)
+
+#Ejercicio de manera correcta 
+
+class CuentaBancaria:
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = saldo
+
+    def retirar(self, cantidad):
+        if cantidad <= 0:
+            print('La cantidad debe ser positiva')
+            return False
+        
+        if cantidad <= self.saldo:
+            self.saldo = self.saldo - cantidad
+            print(f'Retiro exitoso. Nuevo saldo: {self.saldo}')
+            return True
+        else:
+            print('Fondos insuficientes')
+            return False
+
+c = CuentaBancaria('Ana', 100)
+c.retirar(150)  
+c.retirar(50) 
+
+#Adicional a ello se le da al cliente que cuando vay a realizar una compra
+#confirme el valor de la compra 
+
+class Confirmacion:  
+    def confirmacion_compra(self, titular, saldo, comparar): 
+        self.titular = titular
+        self.saldo = saldo
+        self.comparar = comparar     
+
+        if self.comparar <= self.saldo: 
+            print(f'Su compra es de un valor de: {self.comparar} ¿desea continuar?')
+            return True
+        else:
+            print(f'Su compra supera el saldo disponible: {self.saldo}')
+            return False
+
+conf = Confirmacion()
+resultado = conf.confirmacion_compra("Ana", 100, 50) 
+
+resultado = conf.confirmacion_compra("Ana", 100, 150)  
+print(resultado) 
